3f85f3e0b58654f7bdee07b915190fde8aa1ab26
XiaofengT <xft98716@gmail.com>
2023-04-11 16:34:40 +0800

    Add function of determining if the input point is in bounding box

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ solent_itn_json_1 = os.path.join('flood_emergency_planning', 'Material', 'itn', 
 solent_itn_json = os.path.join('flood_emergency_planning', 'Material', 'itn', 'solent_itn.json')
 dataset = rasterio.open(os.path.join('flood_emergency_planning', 'Material', 'elevation', 'SZ.asc'))
 
+
 def input_point():
     # User input point in British National Grid
     lat_input = input('Please enter latitude: ')
@@ -43,9 +44,20 @@ def input_point():
     return input_osgb
 
 
+# Determine if the point is in bounding box
+def is_inside_bound(coord):
+    if 430000 <= coord[0] <= 465000:
+        if 80000 <= coord[1] <= 95000:
+            return True
+        else:
+            return False
+    else:
+        return False
+
 
 def main():
     input_osgb = input_point()
+
 
 if __name__ == '__main__':
     main()
